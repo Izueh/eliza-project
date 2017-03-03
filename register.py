a51e5159225089fda3d0fe979a38fbe439312b10
Manan-Singh <manan.shine@gmail.com>
2017-03-02 22:39:14 -0500

    deleted a lot of files

--- register.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, PasswordField
-
-class RegisterForm(FlaskForm):
-    username = StringField('username')
-    password = PasswordField('password')
-    captcha = RecaptchaField()
